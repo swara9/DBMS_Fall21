9142e2763afc1362220a807e111820354a5e4ad5
Ashutosh Yadav <ash.y.012000@gmail.com>
2021-11-29 02:32:42 -0500

    percentChange

--- Backend/server.js
+++ Backend/server.js
@@ -80,7 +80,7 @@ try{
     ch.close, 
     DECODE(rownum,1, 0, ROUND(change/LAG(close, 1, 0 ) OVER (ORDER BY sh_date)*100, 3)) as percent_change
     FROM (select sh.*, Decode(rownum, 1, 0, close - LAG(close, 1, 0 ) OVER (ORDER BY sh_date))  as change
-        FROM (select sh_date, close from stock_history where isin = 'US0185811082' order by sh_date) sh) ch) pch
+        FROM (select sh_date, close from stock_history where isin = '${ISIN}' order by sh_date) sh) ch) pch
     `
   }
 
