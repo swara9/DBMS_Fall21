92ae5c891744a9c5fbd83bd4f2fdf2ce24016a9d
Ashutosh Yadav <ash.y.012000@gmail.com>
2021-11-30 00:22:07 -0500

    Endpoint file

--- Backend/server.js
+++ Backend/server.js
@@ -57,16 +57,7 @@ try{
   }
   else if(flag==4){
     const { ISIN } = req.body;
-    query=
-    `select pch.*, 
-    ROUND(AVG(percent_change) OVER(ORDER BY sh_date ROWS BETWEEN 10 PRECEDING AND CURRENT ROW),3) as avg_change
-    FROM
-    (select ch.sh_date, 
-    ch.close, 
-    DECODE(rownum,1, 0, ROUND(change/LAG(close, 1, 0 ) OVER (ORDER BY sh_date)*100, 3)) as percent_change
-    FROM (select sh.*, Decode(rownum, 1, 0, close - LAG(close, 1, 0 ) OVER (ORDER BY sh_date))  as change
-        FROM (select sh_date, close from STOCK_HISTORY_WEEKLY where isin = '${ISIN}' order by sh_date) sh) ch) pch
-    `
+    query=queries.percent_change.replace("${ISIN}", ISIN);
   }
 
 
@@ -95,6 +86,6 @@ try{
 });
 
 app.post('/getStockHistory',(req, res) => {conn(1,req, res)});
-app.post('/stockDetails',(req, res) => {conn(2,req, res)});
+app.post('/getStockDetails',(req, res) => {conn(2,req, res)});
 app.post('/getUser',(req, res) => {conn(3,req, res)});
-app.post('/percentChange',(req, res) => {conn(4,req, res)});
+app.post('/getPercentChange',(req, res) => {conn(4,req, res)});
--- Backend/sql/queries.js
+++ Backend/sql/queries.js
@@ -1,4 +1,15 @@
 module.exports = {
     "getStockHistory" : "SELECT sh_date, round(open, 2), round(high,2), round(low, 2), round(close,2) \
-     FROM STOCK_HISTORY_WEEKLY where ISIN='${ISIN}' ORDER BY sh_date ASC"
+     FROM STOCK_HISTORY_WEEKLY where ISIN='${ISIN}' ORDER BY sh_date ASC",
+
+
+     "percent_change" : "select pch.sh_date, ROUND(AVG(percent_change) OVER(ORDER BY sh_date ROWS BETWEEN 10 PRECEDING AND CURRENT ROW),3) as avg_change \
+     FROM \
+     (select ch.sh_date, \
+     ch.close, \
+     DECODE(rownum,1, 0, ROUND(change/LAG(close, 1, 0 ) OVER (ORDER BY sh_date)*100, 3)) as percent_change\
+     FROM (select sh.*, Decode(rownum, 1, 0, close - LAG(close, 1, 0 ) OVER (ORDER BY sh_date))  as change\
+         FROM (select sh_date, close from STOCK_HISTORY_WEEKLY where isin = '${ISIN}' order by sh_date) sh) ch) pch"
+
+    
 }
