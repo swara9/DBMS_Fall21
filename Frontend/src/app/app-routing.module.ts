180c28bdb65a3b1f09b4a5777ae76de4f72e96c9
Swara Lawande <swara.lawande9@gmail.com>
2021-12-01 02:08:11 -0500

    mage changes to urls

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,18 +6,18 @@ import { StocksComponent } from './components/stocks/stocks.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { TradehistoryComponent } from './components/tradehistory/tradehistory.component';
-import { TradeComponent } from './components/trade/trade.component';
 import { ChartModalComponent } from './components/chart-modal/chart-modal.component';
+import { TradeModalComponent } from './components/trade-modal/trade-modal.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: DashboardComponent },
   { path: 'stocks', component: StocksComponent },
-  { path: 'pro', component:ProfileComponent},
-  { path: 'portf', component:PortfolioComponent },
-  { path: 'th', component:TradehistoryComponent },
-  { path: 'trade', component:TradeComponent },
-  { path: 'chart', component:ChartModalComponent}
+  { path: 'profile', component:ProfileComponent},
+  { path: 'portfolio', component:PortfolioComponent },
+  { path: 'tradehistory', component:TradehistoryComponent },
+  { path: 'chart', component:ChartModalComponent},
+  { path: 'trade', component:ChartModalComponent}
 ];
 
 @NgModule({
