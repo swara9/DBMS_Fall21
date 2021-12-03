b5220f9a5ad891dbabe941716225e0b69362735f
Swara Lawande <swara.lawande9@gmail.com>
2021-12-02 23:20:21 -0500

    some changes

--- Frontend/src/app/components/sinfo/sinfo.component.ts
+++ Frontend/src/app/components/sinfo/sinfo.component.ts
@@ -1,5 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '../../services/http-service.service';
+
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexYAxis,
+  ApexXAxis,
+  ApexTitleSubtitle,
+  ApexDataLabels,
+  ApexFill,
+  ApexMarkers,
+  ApexTooltip,
+  ApexLegend,
+  ApexGrid,
+  ApexStroke,
+  ApexAnnotations
+} from "ng-apexcharts";
+import { SortController } from 'ag-grid-community';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  title: ApexTitleSubtitle;  
+};
 
 @Component({
   selector: 'app-sinfo',
@@ -8,10 +36,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SInfoComponent implements OnInit {
 
+  @ViewChild("chart")
+  chart: ChartComponent = new ChartComponent;
+  public chartOptions: Partial<ChartOptions> | any;
   symbol:any;
+  history: any[]= [];
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private http: HttpService
   ) { }
 
   ngOnInit(): void {
@@ -19,6 +52,34 @@ export class SInfoComponent implements OnInit {
       this.symbol = params.get('symbol');
       console.log(this.symbol)
     })
+
+    var isin = 'US0378331005';
+    var symbol = 'ABCD';
+    this.http.getStockHistory(isin)
+    .subscribe(history => {
+      this.history = history
+      this.initHistory(history);
+      // console.log(history)
+    });
+  }    
+
+  initHistory(historySeries: any[][]){
+    console.log(historySeries[0][0].substring(0,10))
+    console.log(historySeries.length+" LENGTH")
+    for(var entry of historySeries){
+      // console.log(entry[0])
+      // let date = new Date(entry[0].substring(0,10));
+      // console.log(date)
+      let ohlc = [entry[1], entry[2], entry[3], entry[4]];
+      let listObj = {
+        // x: date,
+        y: ohlc
+      }      
+      this.history.push(listObj);
+      // console.log(this.history)
+    }
   }
+
+
 
 }
